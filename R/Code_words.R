0bbfd4ab9df38873f4f40865292d238c2f4aa4a5
Anshu Aggarwal <anshu@aggarwal-family.com>
2025-03-08 17:03:01 +0000

    Testing environment variables for non-existing

--- R/Code_words.R
+++ R/Code_words.R
@@ -8,8 +8,9 @@
 #' @export Code_words
 
 Code_words = function(data, text = "word", more2na = T, parallelize = T, print =T, debug=F){
-  crap <- Sys.getenv("WNHOME")
-  cat("WNHOME is ", crap)
+  crap <- Sys.getenv("GARBAGE")
+  cat("Garbage is ", crap)
+  stopifnot(FALSE)
   dataunique = unique(data[[text]])
   dataunique = data.frame(values = dataunique, stringsAsFactors = F)
   message("preprocessing...")
